b6a45693c66f0a241cf16588cbe25a532b4523fd
Matheus <matheussquiles@gmail.com>
2026-01-16 20:19:40 -0300

    AGORA VAIgit add .!

--- buscar_personagem.py
+++ buscar_personagem.py
@@ -46,6 +46,15 @@ params = {
     "order_direction": "1"
 }
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Referer": "https://www.tibia.com/",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1"
+}
+
 response = requests.get(
     BASE_URL,
     params=params,
